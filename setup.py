9307108a2229f49402da1c99475f591657dcb792
vivekananda <vivekanandasahoo34@gmail.com>
2025-12-22 20:18:37 +0530

    The pip install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vivekananda sahoo",
+    author_email="vivekanandasahoo34@gmail.com",
+    packages=find_packages()
+)
